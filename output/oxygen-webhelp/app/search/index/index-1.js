10a7d955bdae8f23bc731766b9ce764f498aeaa5
Ewa-Boloz <92880229+Ewa-Boloz@users.noreply.github.com>
2022-03-13 11:44:59 +0100

    new commit

---
+++ output/oxygen-webhelp/app/search/index/index-1.js
@@ -0,0 +1,4 @@
+define(function () {
+// Auto generated index for searching.
+return {"bołoz":"4*0*a","context":"3*20*0","description":"0*10*-1$8,1*10*-1$a,4*2*g$s$1g","ewa":"4*0*9","first":"0*33*1$4$9$e$j,1*31*1$4$e,2*31*1$4$9,4*60*1$4$7$c$i$l$o$t$12$17$1a$1k$1n$1s","firts":"1*10*-1$8,4*0*1e","help":"3*20*2"};
+});
