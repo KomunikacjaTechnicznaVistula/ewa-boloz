8aa2934c1e20132271aa5f25d67809c8c8f75538
Ewa-Boloz <92880229+Ewa-Boloz@users.noreply.github.com>
2022-03-26 09:56:25 +0100

    change

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,11 +1,21 @@
-define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3","p5"]},{w:"first",p:["p0","p1","p2","p5"]},{w:"Topic",p:["p0"]},{w:"reference",p:["p1"]},{w:"task",p:["p2","p3"]},{w:"second",p:["p3"]},{w:"Context",p:["p4"]},{w:"Sensitive",p:["p4"]},{w:"Help",p:["p4"]},{w:"map",p:["p5"]}];
+define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p13","p15"]},{w:"first",p:["p0","p2","p5","p15"]},{w:"Topic",p:["p0"]},{w:"eleventh",p:["p1"]},{w:"task",p:["p1","p3","p4","p5","p7","p8","p9","p10","p11","p12","p13"]},{w:"reference",p:["p2"]},{w:"eighth",p:["p3"]},{w:"fifth",p:["p4"]},{w:"fourth",p:["p6"]},{w:"taks",p:["p6"]},{w:"ninth",p:["p7"]},{w:"second",p:["p8"]},{w:"seventh",p:["p9"]},{w:"sixth",p:["p10"]},{w:"tenth",p:["p11"]},{w:"t_my_third",p:["p12"]},{w:"tvelfth",p:["p13"]},{w:"Context",p:["p14"]},{w:"Sensitive",p:["p14"]},{w:"Help",p:["p14"]},{w:"map",p:["p15"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
-ph["p1"]=[0, 1, 3];
-ph["p2"]=[0, 1, 4];
-ph["p3"]=[0, 5, 4];
-ph["p4"]=[6, 7, 8];
-ph["p5"]=[0, 1, 9];
+ph["p1"]=[0, 3, 4];
+ph["p2"]=[0, 1, 5];
+ph["p3"]=[0, 6, 4];
+ph["p4"]=[0, 7, 4];
+ph["p5"]=[0, 1, 4];
+ph["p6"]=[0, 8, 9];
+ph["p7"]=[0, 10, 4];
+ph["p8"]=[0, 11, 4];
+ph["p9"]=[0, 12, 4];
+ph["p10"]=[0, 13, 4];
+ph["p12"]=[15, 4];
+ph["p11"]=[0, 14, 4];
+ph["p14"]=[17, 18, 19];
+ph["p13"]=[0, 16, 4];
+ph["p15"]=[0, 1, 20];
      return {
          keywords: keywords,
          ph: ph
